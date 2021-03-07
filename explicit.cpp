3dfb11aacc6b2af91acf2e3a415d86f23ebe452d
John Tran <johnleetran@yahoo.com>
2021-03-07 15:10:07 -0800

    faster using explicity version

--- explicit.cpp
+++ explicit.cpp
@@ -1,6 +1,31 @@
-#include <math.h>   // smallpt, a Path Tracer by Kevin Beason, 2009
-#include <stdlib.h> // Make : g++ -O3 -fopenmp explicit.cpp -o explicit
+#include <math.h>   // smallpt, a Path Tracer by Kevin Beason, 2008
+#include <stdlib.h> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
 #include <stdio.h>  //        Remove "-fopenmp" for g++ version < 4.2
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "nlohmann_json.hpp"
+// for convenience
+using json = nlohmann::json;
+using namespace std;
+#if __EMSCRIPTEN__
+#include <emscripten/bind.h>
+#include <emscripten/val.h>
+#include <emscripten/emscripten.h>
+
+EM_JS(void, MountFilesystem, (const char *repo_root_path), {
+    // inside of emscripten
+    console.log("MountFilesystem mkdir c++");
+    FS.mkdir("/working");
+
+    console.log("MountFilesystem c++");
+    FS.mount(NODEFS, {root : Module.UTF8ToString(repo_root_path)}, "/working");
+});
+
+#endif //__EMSCRIPTEN__
+
 struct Vec
 {                   // Usage: time ./explicit 16 && xv image.ppm
     double x, y, z; // position, also color (r,g,b)
@@ -46,25 +71,71 @@ struct Sphere
         return (t = b - det) > eps ? t : ((t = b + det) > eps ? t : 0);
     }
 };
-Sphere spheres[] = {
-    //Scene: radius, position, emission, color, material
-    Sphere(1e5, Vec(1e5 + 1, 40.8, 81.6), Vec(), Vec(.75, .25, .25), DIFF),   //Left
-    Sphere(1e5, Vec(-1e5 + 99, 40.8, 81.6), Vec(), Vec(.25, .25, .75), DIFF), //Rght
-    Sphere(1e5, Vec(50, 40.8, 1e5), Vec(), Vec(.75, .75, .75), DIFF),         //Back
-    Sphere(1e5, Vec(50, 40.8, -1e5 + 170), Vec(), Vec(), DIFF),               //Frnt
-    Sphere(1e5, Vec(50, 1e5, 81.6), Vec(), Vec(.75, .75, .75), DIFF),         //Botm
-    Sphere(1e5, Vec(50, -1e5 + 81.6, 81.6), Vec(), Vec(.75, .75, .75), DIFF), //Top
-    Sphere(16.5, Vec(27, 16.5, 47), Vec(), Vec(1, 1, 1) * .999, SPEC),        //Mirr
-    Sphere(16.5, Vec(73, 16.5, 78), Vec(), Vec(1, 1, 1) * .999, REFR),        //Glas
-    Sphere(1.5, Vec(50, 81.6 - 16.5, 81.6), Vec(4, 4, 4) * 100, Vec(), DIFF), //Lite
-};
-int numSpheres = sizeof(spheres) / sizeof(Sphere);
+
+json read_json_file(std::string file_path)
+{
+    std::ifstream i(file_path);
+    json j;
+    i >> j;
+    return j;
+}
+
+Refl_t extract_reflection_type(std::string r_type)
+{
+    Refl_t reflection_type;
+    if (r_type == "DIFF")
+    {
+        reflection_type = DIFF;
+    }
+    else if (r_type == "SPEC")
+    {
+        reflection_type = SPEC;
+    }
+    else
+    {
+        reflection_type = REFR;
+    }
+    return reflection_type;
+}
+
+std::vector<Sphere> spheres;
+void load_scene(std::string scene_data)
+{
+    std::cout << "load_scene(std::string scene_data)" << std::endl;
+    std::cout << scene_data << std::endl;
+    json data = json::parse(scene_data); //read_json_file(scene_file);
+
+    std::cout << "dataSpheres" << std::endl;
+    for (auto &sphere : data["Spheres"])
+    {
+        std::cout << "------------------------------------" << std::endl;
+
+        int radius = sphere["radius"];
+        std::cout << "radius: " << radius << std::endl;
+
+        Vec position{sphere["poistion"][0], sphere["poistion"][1], sphere["poistion"][2]};
+        std::cout << "position: " << sphere["poistion"] << std::endl;
+
+        Vec emission{sphere["emission"][0], sphere["emission"][1], sphere["emission"][2]};
+        std::cout << "emission: " << sphere["emission"] << std::endl;
+
+        Vec color{sphere["color"][0], sphere["color"][1], sphere["color"][2]};
+        std::cout << "color: " << sphere["color"] << std::endl;
+
+        Refl_t reflection_type = extract_reflection_type(sphere["reflection_type"]);
+        std::cout << "reflection_type: " << sphere["reflection_type"] << std::endl;
+
+        spheres.push_back(Sphere(radius, position, emission, color, reflection_type));
+    }
+}
+
 inline double clamp(double x) { return x < 0 ? 0 : x > 1 ? 1
                                                          : x; }
 inline int toInt(double x) { return int(pow(clamp(x), 1 / 2.2) * 255 + .5); }
 inline bool intersect(const Ray &r, double &t, int &id)
 {
-    double n = sizeof(spheres) / sizeof(Sphere), d, inf = t = 1e20;
+    int n = spheres.size();
+    double d, inf = t = 1e20;
     for (int i = int(n); i--;)
         if ((d = spheres[i].intersect(r)) && d < t)
         {
@@ -75,6 +146,7 @@ inline bool intersect(const Ray &r, double &t, int &id)
 }
 Vec radiance(const Ray &r, int depth, unsigned short *Xi, int E = 1)
 {
+    int numSpheres = spheres.size();
     double t;   // distance to intersection
     int id = 0; // id of intersected object
     if (!intersect(r, t, id))
@@ -83,11 +155,14 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi, int E = 1)
     Vec x = r.o + r.d * t, n = (x - obj.p).norm(), nl = n.dot(r.d) < 0 ? n : n * -1, f = obj.c;
     double p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y
                                                         : f.z; // max refl
-    if (++depth > 5 || !p)
-        if (erand48(Xi) < p)
+    if (++depth > 5 || !p){
+        if (erand48(Xi) < p){
             f = f * (1 / p);
-        else
+        }else{
             return obj.e * E;
+        }
+    }
+
     if (obj.refl == DIFF)
     { // Ideal DIFFUSE reflection
         double r1 = 2 * M_PI * erand48(Xi), r2 = erand48(Xi), r2s = sqrt(r2);
@@ -119,8 +194,9 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi, int E = 1)
 
         return obj.e * E + e + f.mult(radiance(Ray(x, d), depth, Xi, 0));
     }
-    else if (obj.refl == SPEC) // Ideal SPECULAR reflection
+    else if (obj.refl == SPEC) {
         return obj.e + f.mult(radiance(Ray(x, r.d - n * 2 * n.dot(r.d)), depth, Xi));
+    }
     Ray reflRay(x, r.d - n * 2 * n.dot(r.d)); // Ideal dielectric REFRACTION
     bool into = n.dot(nl) > 0;                // Ray from outside going in?
     double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc, ddn = r.d.dot(nl), cos2t;
@@ -134,17 +210,20 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi, int E = 1)
                                                        : radiance(Ray(x, tdir), depth, Xi) * TP)
                                     : radiance(reflRay, depth, Xi) * Re + radiance(Ray(x, tdir), depth, Xi) * Tr);
 }
-int main(int argc, char *argv[])
+int render(int argc, char *argv[])
 {
-    int w = 1024, h = 768, samps = argc == 2 ? atoi(argv[1]) / 4 : 1; // # samples
+    int w = 1024, h = 768, samps = argc >= 2 ? atoi(argv[1]) / 4 : 1; // # samples
     Ray cam(Vec(50, 52, 295.6), Vec(0, -0.042612, -1).norm());        // cam pos, dir
     Vec cx = Vec(w * .5135 / h), cy = (cx % cam.d).norm() * .5135, r, *c = new Vec[w * h];
 #pragma omp parallel for schedule(dynamic, 1) private(r) // OpenMP
     for (int y = 0; y < h; y++)
     { // Loop over image rows
-        fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h - 1));
-        for (unsigned short x = 0, Xi[3] = {0, 0, y * y * y}; x < w; x++) // Loop cols
-            for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++)       // 2x2 subpixel rows
+        auto spp_info = samps * 4;
+        auto percent_complete = 100.0 * y / (h - 1);
+        std::cout << "spp_info:" << spp_info << " status: " << percent_complete << std::endl;
+        //fprintf(stderr, "\rRendering (%d spp) %5.2f%%", samps * 4, 100. * y / (h - 1));
+        for (unsigned short x = 0, Xi[3] = {0, 0, static_cast<unsigned short>(y * y * y)}; x < w; x++){
+            for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++) {
                 for (int sx = 0; sx < 2; sx++, r = Vec())
                 { // 2x2 subpixel cols
                     for (int s = 0; s < samps; s++)
@@ -157,9 +236,130 @@ int main(int argc, char *argv[])
                     } // Camera rays are pushed ^^^^^ forward to start in interior
                     c[i] = c[i] + Vec(clamp(r.x), clamp(r.y), clamp(r.z)) * .25;
                 }
+            }
+        }
     }
+
+    std::cout << "write render to file" << std::endl;
+#if __EMSCRIPTEN__
+    FILE *f = fopen("/working/image.ppm", "w"); // Write image to PPM file.
+#else
     FILE *f = fopen("image.ppm", "w"); // Write image to PPM file.
+#endif //__EMSCRIPTEN__
     fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
     for (int i = 0; i < w * h; i++)
+    {
         fprintf(f, "%d %d %d ", toInt(c[i].x), toInt(c[i].y), toInt(c[i].z));
+    }
+    std::cout << "DOne" << std::endl;
+    return 0;
 }
+
+#if __EMSCRIPTEN__
+int main(int argc, char *argv[])
+{
+    std::cout << "hello from wasm" << std::endl;
+
+    /*****
+    //first argv is spp (sample per pixel)
+    //second argv is scene data
+    if(argc < 3){
+        std::cout << "Usage error: ./smallpt <SPP> <SCENE_DATA>" << std::endl;
+        return 1;
+    }
+    ****/
+    std::string ssp = argv[1];
+    std::cout << " argv[1]: " << ssp << std::endl;
+
+    std::string scene_data = argv[2];
+    std::cout << " argv[2]: " << scene_data << std::endl;
+
+    std::string cur_dir = "./";
+    std::cout << "MountFilesystem: " << cur_dir << std::endl;
+    MountFilesystem(cur_dir.c_str());
+
+    std::cout << "load_scene: " << scene_data << std::endl;
+    load_scene(scene_data);
+
+    std::cout << "render" << std::endl;
+    return render(argc, argv);
+}
+#else
+
+json spheresToJson(std::vector<Sphere> spheres)
+{
+    json j;
+    j["Spheres"] = json::array();
+    for (auto &sphere : spheres)
+    {
+        json json_object = json::object();
+        json_object["radius"] = sphere.rad;
+        json_object["poistion"] = json::array({sphere.p.x, sphere.p.y, sphere.p.z});
+        json_object["emission"] = json::array({sphere.e.x, sphere.e.y, sphere.e.z});
+        json_object["color"] = json::array({sphere.c.x, sphere.c.y, sphere.c.z});
+        if (sphere.refl == DIFF)
+        {
+            json_object["reflection_type"] = "DIFF";
+        }
+        else if (sphere.refl == SPEC)
+        {
+            json_object["reflection_type"] = "SPEC";
+        }
+        else
+        {
+            json_object["reflection_type"] = "REFR";
+        }
+        j["Spheres"].push_back(json_object);
+    }
+    return j;
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "hello from wasm" << std::endl;
+
+    /*****
+    //first argv is spp (sample per pixel)
+    //second argv is scene data
+    if(argc < 3){
+        std::cout << "Usage error: ./smallpt <SPP> <SCENE_DATA>" << std::endl;
+        return 1;
+    }
+    ****/
+    std::string ssp = argv[1];
+    std::cout << " argv[1]: " << ssp << std::endl;
+
+    std::string scene_data = argv[2];
+    std::cout << " argv[2]: " << scene_data << std::endl;
+
+    std::cout << "load_scene: " << scene_data << std::endl;
+    load_scene(scene_data);
+
+    std::cout << "render" << std::endl;
+    return render(argc, argv);
+
+    // //double R=60;
+    // double R = 120; // radius
+    // double T = 30 * M_PI / 180.;
+    // double D = R / cos(T); //distance
+    // // double D=60;     //distance
+    // // double R=D*sqrt(2);
+    // double Z = 62;
+    // Vec C = Vec(0.275, 0.612, 0.949);
+    // std::vector<Sphere> spheres = {
+    //     //Scene: radius, position, emission, color, material
+
+    //     Sphere(R, Vec(50, 28, Z) + Vec(cos(T), sin(T), 0) * D, C * 6e-2, Vec(1, 1, 1) * .996, SPEC),                                                                            //red
+    //     Sphere(R, Vec(50, 28, Z) + Vec(-cos(T), sin(T), 0) * D, C * 6e-2, Vec(1, 1, 1) * .996, SPEC),                                                                           //grn
+    //     Sphere(R, Vec(50, 28, Z) + Vec(0, -1, 0) * D, C * 6e-2, Vec(1, 1, 1) * .996, SPEC),                                                                                     //blue
+    //     Sphere(R, Vec(50, 28, Z) + Vec(0, 0, -1) * R * 2 * sqrt(2. / 3.), C * 0e-2, Vec(1, 1, 1) * .996, SPEC),                                                                 //back
+    //                                                                                                                                                                             //  Sphere(1e5, Vec(50,28,Z)+Vec(0,0,1e5+170),   Vec(1,1,1)*0,Vec(1,1,1)*.996, SPEC), //front
+    //                                                                                                                                                                             //  Sphere(2*R*2*sqrt(2./3.)-R*2*sqrt(2./3.)/3., Vec(50,28,Z)+Vec(0,0,-R*2*sqrt(2./3.)/3.),   Vec(1,1,1)*0,Vec(1,1,1)*.3333, SPEC), //front
+    //     Sphere(2 * 2 * R * 2 * sqrt(2. / 3.) - R * 2 * sqrt(2. / 3.) / 3., Vec(50, 28, Z) + Vec(0, 0, -R * 2 * sqrt(2. / 3.) / 3.), Vec(1, 1, 1) * 0, Vec(1, 1, 1) * .5, SPEC), //front
+    // };
+
+    // json j = spheresToJson(spheres);
+
+    // std::cout << j << std::endl;
+}
+#endif //__EMSCRIPTEN__
